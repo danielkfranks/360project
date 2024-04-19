21c6f749dca797409c98906cc3e362b49c4ef13e
danielkfranks <danielkfranks@gmail.com>
2024-04-18 23:43:44 -0700

    started js work

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+  define("DB_HOST", "localhost");
+  define("DB_NAME", "NoDD");
+  define("DB_CHARSET", "utf8mb4");
+  define("DB_USER", "root");
+  define("DB_PASSWORD", "");
+
+  $pdo = new PDO(
+	  "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET, DB_USER, DB_PASSWORD, [
+		  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+	  ]
+  );
+
+  $stmt = $pdo->prepare("SELECT 'A1' from 'Problems'");
+  $stmt -> execute();
+  $out = $stmt -> fetchAll();
+  printf("<div>%u</div>", $out);
